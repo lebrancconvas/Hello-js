18a30cc4cfd8e233384fe89d5803ad9e635251d0
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2017-11-27 18:52:20 +0700

    What is your name ?

---
+++ .gitignore/What is your name.js
@@ -0,0 +1,8 @@
+var name = prompt('My name is : ');
+if (name === 'admin'){
+	alert('WTF! , You are admin!!!');
+} else if (name === 'user'){
+	alert('WTF! , You are user!!!')
+} else {
+	alert('Your name is ' + name + ' , Right?');
+}
